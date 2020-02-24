25f17c9e244513197d229fbf8a41793129419f36
delightful-source-3050 <joshmrallen@gmail.com>
2020-02-24 16:56:04 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,10 +1,7 @@
 def find_element_index(array, value_to_find)
   # Add your solution here
   
-  if array.index(value_to_find)
-    return array.index(value_to_find)
-  else 
-  
+  array.index(value_to_find)
   
 end
 
